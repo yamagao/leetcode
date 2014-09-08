af1677eec9af497f021245b6899b719f2b6e7dca
yamagao <yamagao@126.com>
2014-09-08 12:33:22 -0400

    2nd update

--- Reverse Integer.java
+++ Reverse Integer.java
@@ -1,16 +1,12 @@
 public class Solution {
     public int reverse(int x) {
-        int count = 0;
-        int result = 0;
-        int expo;
-        int test = x;
-        while(test / 10 != 0){
-            test /= 10;
-            count++;
+        if(x == 0){
+            return 0;
         }
-        for(int i = 0; i <= count; i++){
-            expo = count - i;
-            result += (Math.pow(10, expo)) * (x % 10);
+        int result = x % 10;
+        x /= 10;
+        while(x != 0){
+            result = result * 10 + x % 10;
             x /= 10;
         }
         return result;
